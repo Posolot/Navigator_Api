2a2be95ad122609d14010d2d105873a687132fc6
ilushenka <saharokbro@mail.ru>
2025-03-11 21:05:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/navigator_api/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,31 @@
+package com.example.navigator_api.ui.gallery
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.navigator_api.R
+
+class GalleryFragment : Fragment() {
+
+    private val galleryViewModel: GalleryViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_gallery, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val textView: TextView = view.findViewById(R.id.text_gallery)
+        galleryViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+    }
+}
---
+++ app/src/main/java/com/example/navigator_api/ui/slideshow/SlideshowFragment.kt
@@ -0,0 +1,30 @@
+package com.example.navigator_api.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.navigator_api.R
+
+class SlideshowFragment : Fragment() {
+
+    private val slideshowViewModel: SlideshowViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_slideshow, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val textView: TextView = view.findViewById(R.id.text_slideshow)
+        slideshowViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+    }
+}
